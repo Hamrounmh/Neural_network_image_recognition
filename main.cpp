7c63cf5ab83aa1bb413e2c31c40c76559ae166cd
Hamru Mohouss <hamrounhoussem@gmail.com>
2021-03-12 19:47:13 +0100

    Affichage dans le main de toutes les Iris

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main() {
     //test de l'ouverture de toute les iris:
     for(int i=0;i<150;i++){
         Iris ir = Iris(i);
-        cout<< ir.get_label()<<"--->" <<i <<"\n";
+        cout<< ir.get_label()<<"---->" << '['<<ir[0]<<',' <<ir[1]<<',' << ir[2]<<',' << ir[3]<<']'<< "---->"<<i <<"\n";
     }
 
 
